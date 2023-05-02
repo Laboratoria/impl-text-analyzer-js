84d3adcf682f84830e40efd06354a8a3fdbbfc2a
Sergio Sinuco <ssinuco@gmail.com>
2023-05-02 08:57:14 -0500

    first commit

---
+++ src/analyzer.js
@@ -0,0 +1,100 @@
+const EMPTY = '';
+const SPACE = ' ';
+const PUNCTUATION_MARKS = ['.', ',', ';', ':', '"', '¿', '?', '¡', '!', '-', '«', '»', '[', ']', '{', '}', '(', ')'];
+
+const getWords = (text) => {
+  //get words from text using a for
+  const words = [];
+  let currentWord = '';
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] === SPACE || PUNCTUATION_MARKS.includes(text[i])) {
+      if (currentWord.length > 0) {
+        words.push(currentWord);
+      }      
+      currentWord = '';
+      continue;
+    }
+    currentWord = currentWord + text[i];
+  }
+  if (currentWord.length > 0) {
+    words.push(currentWord);
+  }
+  return words;
+};
+
+const analyzer = {  
+  getWordCount: (text) => {
+    const trimmedText = text.trim();    
+    if(trimmedText.length === 0) {
+      return 0;
+    }
+    const words = getWords(trimmedText);
+
+    return words.length;
+  },
+  getCharacterCount: (text) => {
+    return text.length;
+  },
+  getCharacterCountExcludingSpaces: (text) => {
+    const characters = text.split('');
+    let noSpaceCount = 0;
+    for (let i = 0; i < characters.length; i++) {
+      if (characters[i] !== ' '){
+        if(!PUNCTUATION_MARKS.includes(characters[i])) {
+          noSpaceCount++;
+        }
+      }      
+    }
+    return noSpaceCount;
+  },
+  getAverageWordLength: (text) => {    
+    let totalLength = 0;
+    const words = getWords(text);
+    if(words.length === 0) {
+      return 0;
+    }
+
+    for (let i = 0; i < words.length; i++) {
+      totalLength = totalLength + words[i].length;
+    }
+    const average = totalLength / words.length;
+    // Round to two decimal places
+    return Math.round(average * 100) / 100;
+  },
+  //sum all numbers in a text
+  getNumberCount: (text) => {
+    const words = text.split(' ');
+    let count = 0;
+    for (let i = 0; i < words.length; i++) {
+      if (words[i] === EMPTY) {
+        continue;
+      }
+
+      const number = Number(words[i]);
+      if (!isNaN(number)) {
+        count+=1;
+      }
+    }
+    return count;
+  },
+  //sum all numbers in a text
+  getNumberSum: (text) => {
+    const words = text.split(' ');
+    let sum = 0;
+    for (let i = 0; i < words.length; i++) {
+      if (words[i] === EMPTY) {
+        continue;
+      }
+
+      const number = Number(words[i]);
+      if (!isNaN(number)) {      
+        sum += number;
+      }
+    }
+    // Round to two decimal places
+    return Math.round(sum * 100) / 100;
+  },
+  
+};
+
+export default analyzer;
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import analyzer from './analyzer.js';
+
+const updateMetrics = (text) => {
+  const wordCount = analyzer.getWordCount(text);
+  const characterCount = analyzer.getCharacterCount(text);
+  const averageWordLength = analyzer.getAverageWordLength(text);
+  const numberCount = analyzer.getNumberCount(text);
+  const numberSum = analyzer.getNumberSum(text);
+  const characterCountExcludingSpaces = analyzer.getCharacterCountExcludingSpaces(text);  
+  document.querySelector('li.metric:nth-child(1)').textContent = "Caracteres: " + characterCount;
+  document.querySelector('li.metric:nth-child(2)').textContent = "Caracters Sin Espacios:" + characterCountExcludingSpaces;  
+  document.querySelector('li.metric:nth-child(3)').textContent = "Palabras:" + wordCount;
+  document.querySelector('li.metric:nth-child(4)').textContent = "Números: " + numberCount;
+  document.querySelector('li.metric:nth-child(5)').textContent = "Suma números: " + numberSum;
+  document.querySelector('li.metric:nth-child(6)').textContent = "Promedio longitud: " + averageWordLength;
+};
+
+const textarea = document.querySelector('textarea[name="user-input"]');
+textarea.addEventListener('keyup', () => {
+  const text = textarea.value;
+  updateMetrics(text);
+});
+
+const button = document.getElementById('update-button');
+button.addEventListener('click', () => {
+  textarea.value = '';
+  updateMetrics('');
+});
+
